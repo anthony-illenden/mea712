28319a5949ab3a89fc048ce09ad1e8a575980678
Tony Illenden <tonyillenden@gmail.com>
2025-08-21 10:14:49 -0400

    minor changes

--- hw/mini_hw2/miniHW2.f90
+++ hw/mini_hw2/miniHW2.f90
@@ -2,13 +2,13 @@ program mini_hw2
 implicit none
 
 ! Define parameters 
-integer, parameter :: nx = 21
-integer, parameter :: nt = 10
-real, parameter :: dx = 100
-real, parameter :: dt = 10.0
-real, dimension(nx) :: psi
+integer, parameter :: nx = 21 ! number of points on the domain
+integer, parameter :: nt = 10 ! number of time steps
+real, parameter :: dx = 100 ! length of spacing between points on the domain
+real, parameter :: dt = 10.0 ! length of time steps
+real, dimension(nx) :: psi 
 real, dimension(nx) :: psif
-real, parameter :: c = 10.0
+real, parameter :: c = 10.0 ! current of constant speed
  
 ! Define indecies
 integer :: i
